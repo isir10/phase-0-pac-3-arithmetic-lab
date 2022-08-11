8e23d20b82f6d703fc9f5f90dec02cb0aa4977ab
isir10 <isir.malak@moringaschool.com>
2022-08-11 16:15:15 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,31 @@
+function add(i, m){
+    return i +m;
+}
+
+function subtract(i, m){
+    return i - m;
+}
+
+function multiply(i, m){
+    return i * m;
+}
+
+function divide(i, m){
+    return i / m;
+}
+
+function increment(n){
+    return n+= 1;
+}
+
+function decrement(n){
+    return n -= 1;
+}
+
+function makeInt(n){
+
+    return parseInt(n, 10);
+}
+function preserveDecimal(n){
+    return parseFloat(n);
+}
